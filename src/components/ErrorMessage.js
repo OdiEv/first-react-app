01ca6ee8259ebdc959915447d5d8d633f9e074a8
OdiEv <odinetszhenya@gmail.com>
2019-05-27 01:46:53 +0300

    day 2

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../error.png';
+
+const ErrorMessage = () => {
+	return(
+		<>
+			<img alt="error" src={img} />
+		</>
+	)
+}
+
+export default ErrorMessage;
---
+++ src/components/ErrorMessageUsers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../error.png';
+
+const ErrorMessageUsers = () => {
+	return(
+		<>
+			<img className="error" alt="error" src={img} />
+		</>
+	)
+}
+
+export default ErrorMessageUsers;
