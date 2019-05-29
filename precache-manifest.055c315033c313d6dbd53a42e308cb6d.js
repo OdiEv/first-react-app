a5cdb2f995fd3bf2802a28f85c893ca18182abeb
OdiEv <odinetszhenya@gmail.com>
2019-05-29 15:26:27 +0300

    Updates

---
+++ precache-manifest.055c315033c313d6dbd53a42e308cb6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193012381fb080f16d07e42b18d8824f",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "2b6827e1d0e6c33ef9de",
+    "url": "/first-react-app/static/css/main.0184bb06.chunk.css"
+  },
+  {
+    "revision": "71a81a8cfb441569c743",
+    "url": "/first-react-app/static/js/2.cca0c4fa.chunk.js"
+  },
+  {
+    "revision": "2b6827e1d0e6c33ef9de",
+    "url": "/first-react-app/static/js/main.6fb3371a.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/first-react-app/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/first-react-app/static/media/logo.9e8a2b39.svg"
+  },
+  {
+    "revision": "cdd35491a11e90ab711aacb8f402c728",
+    "url": "/first-react-app/static/media/spinner.cdd35491.svg"
+  }
+]);
